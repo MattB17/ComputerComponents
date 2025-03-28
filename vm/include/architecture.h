1285fed45065e5a92ed1d4dea9db1da94dcf9d9c
MattB17 <matt.buckley1755@gmail.com>
2025-03-27 22:16:49 -0400

    Adding high level architecture

---
+++ vm/include/architecture.h
@@ -0,0 +1,59 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+// 16-bit machine, each memory location stores a 16-bit value
+// 2^16 = 65536 memory locations
+#define MEMORY_MAX (1 << 16)
+uint16_t mem[MEMORY_MAX];
+
+// Our LC-3 architecture will have 10 registers:
+// - 8 general purpose registers (R_0 - R_7)
+// - a program counter (R_PC)
+// - and a register for condition flags (R_COND)
+enum Register {
+  R_0 = 0,
+  R_1,
+  R_2,
+  R_3,
+  R_4,
+  R_5,
+  R_6,
+  R_7,
+  R_PC,
+  R_COND,
+  R_MAX
+};
+uint16_t regs[R_MAX];
+
+// The R_COND register stores condition flags providing info about
+// the most recently executed calculation.
+// LC-3 only 3 condition flags, indicating whether the previous calculation
+// was positive, zero, or negative.
+enum Condition {
+  FL_POS = 1 << 0,  // Positive
+  FL_ZRO = 1 << 1,  // Zero
+  FL_NEG = 1 << 2   // Negative
+};
+
+// Our instructions will be 16-bits consiting of an opcode followed by
+// parameters.
+// There are 16 opcodes so these will represent the first 4 bits of the
+// instruction.
+enum OpCode {
+  OP_BR = 0,  // Branch
+  OP_ADD,     // Addition
+  OP_LD,      // load
+  OP_ST,      // store
+  OP_JSR,     // jump register
+  OP_AND,     // bitwise AND
+  OP_LDR,     // load register
+  OP_STR,     // store register
+  OP_RTI,     // unused
+  OP_NOT,     // bitwise not
+  OP_LDI,     // load indirect
+  OP_STI,     // store indirect
+  OP_JMP,     // jump
+  OP_RES,     // reserved (unused)
+  OP_LEA,     // load effective address
+  OP_TRAP     // execute trap
+};
